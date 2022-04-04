d41523c4b97c2ed5738290748c32a4282412010c
mrthorpe <mrthorpe@umich.edu>
2022-04-04 18:42:56 -0400

    Link JS to index.  Add variables for play, pause, slow, speed, and skip buttons.  Add variables for video element and for volume level.  Test and implement event listeners for all buttons.

--- js/video.js
+++ js/video.js
@@ -1,11 +1,40 @@
-var video;
+var playButton = document.querySelector("#play");
+var pauseButton = document.querySelector("#pause");
+var slowButton = document.querySelector("#slower");
+var speedButton = document.querySelector("#faster");
+var skipButton = document.querySelector("skip");
+
+var video = document.querySelector("#player1");
+var volumeLevel = document.querySelector("#slider").value;
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
 
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+playButton.addEventListener("click", function() {
+	console.log("Play Video");
+	document.querySelector("#volume").innerHTML = `${volumeLevel}%`;
+	video.play();
+});
+
+pauseButton.addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+})
+
+slowButton.addEventListener("click", function() {
+	video.playbackRate = video.playbackRate * 0.95;
+	console.log(`New speed is ${video.playbackRate}`);
+})
+
+speedButton.addEventListener("click", function() {
+	video.playbackRate = video.playbackRate * 1.05;
+	console.log(`New speed is ${video.playbackRate}`);
+})
+
+skipButton.addEventListener("click", function() {
+	video.currentTime += 15;
+	console.log(video.currentTime);
+})
 
